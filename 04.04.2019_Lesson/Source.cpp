893833d044b33dd70981810969c4c8ecdee4ca18
айкеновад <@205_8>
2019-04-04 08:55:55 +0600

    Добавьте файлы проекта.

---
+++ 04.04.2019_Lesson/Source.cpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include<iostream>
+#include <iomanip>
+#include<Windows.h>
+#include<ctime>
+#include<cstring>
+#include<fstream>
+// НЕЛЬЗЯ В ЗАГОЛОВОЧНЫХ ФАЙЛАХ ПРОСТО ТАК ПЕРЕПОДКЮЧАТЬ ДРУГИЕ ЗАГОЛОВКИ - там заморочки с pragma
+using namespace std;
